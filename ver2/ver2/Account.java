4bb1f7fc6d9d79358c3733adadbb7914fed75a25
JKleyRod21 <jordenrodriguez06@gmail.com>
2025-09-02 11:48:03 -0400

    Removed constructor

--- ver2/ver2/Account.java
+++ ver2/ver2/Account.java
@@ -7,10 +7,6 @@ public class Account {
 	
 	private String name;
 	
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
-
 	public Account(String name, double balance) {
 		this.name = name;
 		this.balance = balance;
